335e964100ce4e09dc01ceddeacefd69e2b5cb42
James Coombes <jacoombes22@Jamess-MacBook-Air.local>
2026-06-12 12:55:09 -0500

    naming issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Set base to your GitHub Pages repo name, e.g. '/world-cup-draft-2026/'
+// Set base to your GitHub Pages repo name, e.g. '/world-cup-gtj/'
 // Use '/' if deploying to a custom domain or username.github.io root.
 export default defineConfig({
   plugins: [react()],
-  base: '/world-cup-draft-2026/'
+  base: '/world-cup-gtj/'
 });
